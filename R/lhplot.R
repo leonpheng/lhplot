e87bd46bf75b7a2a9fabed23459009d59aaea672
Leon Pheng <Leon.Pheng@certara.com>
2021-01-30 11:47:58 -0500

    initial commit

--- R/lhplot.R
+++ R/lhplot.R
@@ -479,7 +479,8 @@ lh_cat_cov<-function(data=cateta,lst.eta=keta,lst.cov=cat,save.path=NULL,fancy="
 #' @examples p1<-lh_con_cov(data=cateta,lst.eta=keta,lst.cov=cat,save.path=NULL)
 
 lh_con_cov<-function(data=coneta,lst.eta=keta,lst.cov=conv,save.path="./scatter.png",fancy="yes"){
-  library(ggpubr)
+  library(grid)
+  require(gpairs)
   if(!is.null(fancy)){
     names(data)[names(data)%in%lst.eta]<-gsub("ETA","",names(data)[names(data)%in%lst.eta])
     lst.eta<-gsub("ETA","",lst.eta)
@@ -710,8 +711,7 @@ gpairs<-function (x, upper.pars = list(scatter = "points", conditional = "barcod
                   stripplot.pars = NULL, barcode.pars = NULL, mosaic.pars = NULL,
                   axis.pars = NULL, diag.pars = NULL, whatis = FALSE)
 {
-  library(grid)
-  require(gpairs)
+
   if (!is.data.frame(x)) {
     if (is.matrix(x))
       x <- as.data.frame(x)
